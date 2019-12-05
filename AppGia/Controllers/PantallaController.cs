9bf8234f128315c01a22ee8245894433d0c9019b
Freddy Seseña Dominguez <42950131+inzunsaNull@users.noreply.github.com>
2019-12-05 13:18:16 -0600

    Commit se agrego Controller y DataAccess de Pantalla

---
+++ AppGia/Controllers/PantallaController.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using Microsoft.AspNetCore.Mvc;
+using AppGia.Models;
+
+namespace AppGia.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class PantallaController : ControllerBase
+    {
+        PantallaDataAccessLayer objPantalla = new PantallaDataAccessLayer();
+        // GET: api/Centro
+        [HttpGet]
+        public IEnumerable<Pantalla> Get()
+        {
+            return objPantalla.GetAllPantallas();
+        }
+
+        //// GET: api/Centro/5
+        //[HttpGet("{id}", Name = "")]
+        //public string Get(int id)
+        //{
+        //    return "value";
+        //}
+
+        // POST: api/Centro
+        [HttpPost]
+        public int Create([FromBody] Pantalla pantalla)
+        {
+            return objPantalla.addPantalla(pantalla);
+        }
+
+        //// PUT: api/Centro/5
+        //[HttpPut("{id}")]
+        //public void Put(int id, [FromBody] string value)
+        //{
+        //}
+
+        //// DELETE: api/ApiWithActions/5
+        //[HttpDelete("{id}")]
+        //public void Delete(int id)
+        //{
+        //}
+    }
+}
---
+++ AppGia/Controllers/PantallaDataAccessLayer.cs
@@ -0,0 +1,67 @@
+﻿using System.Collections.Generic;
+using AppGia.Models;
+using Npgsql;
+
+
+namespace AppGia.Controllers
+{
+    public class PantallaDataAccessLayer
+    {
+        private string connectionString = "User ID=postgres;Password=HolaMundo1;Host=192.168.1.73;Port=5432;Database=GIA;Pooling=true;";
+        char cod = '"';
+        public IEnumerable<Pantalla> GetAllPantallas()
+        {
+            string cadena = "SELECT * FROM" + cod + "TAB_PANTALLA" + cod + "";
+            try
+            {
+                List<Pantalla> lstpantalla = new List<Pantalla>();
+
+                using (NpgsqlConnection con = new NpgsqlConnection(connectionString))
+                {
+                    NpgsqlCommand cmd = new NpgsqlCommand(cadena, con);
+                    con.Open();
+                    NpgsqlDataReader rdr = cmd.ExecuteReader();
+
+                    while (rdr.Read())
+                    {
+                        Pantalla pantalla = new Pantalla();
+                        pantalla.STR_NOMBRE_PANTALLA = rdr["STR_NOMBRE_PANTALLA"].ToString();
+
+                        lstpantalla.Add(pantalla);
+                    }
+                    con.Close();
+                }
+
+                return lstpantalla;
+            }
+            catch
+            {
+                throw;
+            }
+        }
+
+        public int addPantalla(Pantalla pantalla)
+        {
+            string add = "INSERT INTO" + cod + "TAB_PANTALLA" + cod + "(" + cod + "STR_NOMBRE_PANTALLA" + cod + ") VALUES " +
+                "(@STR_NOMBRE_PANTALLA)";
+            try
+            {
+                using (NpgsqlConnection con = new NpgsqlConnection(connectionString))
+                {
+                    NpgsqlCommand cmd = new NpgsqlCommand(add, con);
+                    //cmd.Parameters.AddWithValue("@INT_IDGRUPO", grupo.INT_IDGRUPO);
+                    cmd.Parameters.AddWithValue("@STR_NOMBRE_PANTALLA", pantalla.STR_NOMBRE_PANTALLA);
+
+                    con.Open();
+                    cmd.ExecuteNonQuery();
+                    con.Close();
+                }
+                return 1;
+            }
+            catch
+            {
+                throw;
+            }
+        }
+    }
+}
