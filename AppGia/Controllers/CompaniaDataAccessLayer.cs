0d6383fba4a664222fe0c5f1da11d2e185502979
Master <proyecto.giavector@gmail.com>
2019-12-05 16:02:35 -0600

    controller de compania metodos: obtener y agregar

--- AppGia/Controllers/CompaniaDataAccessLayer.cs
+++ AppGia/Controllers/CompaniaDataAccessLayer.cs
@@ -1,10 +1,9 @@
 ﻿using System;
 using System.Collections.Generic;
-<<<<<<< HEAD
+
 using System.Linq;
 using System.Threading.Tasks;
-=======
->>>>>>> 66851b5e273fa8258515a35d3f2489ba07263c5e
+
 using AppGia.Models;
 using Npgsql;
 
@@ -14,37 +13,24 @@ namespace AppGia.Controllers
     {
         private string connectionString = "User ID=postgres;Password=HolaMundo1;Host=192.168.1.73;Port=5432;Database=GIA;Pooling=true;";
         char cod = '"';
-<<<<<<< HEAD
 
-        public IEnumerable<Compania> GetAllCompanias()
-        {
-            string consulta = "SELECT * FROM" + cod + "CentroCostos" + cod + "";
-            try
-            {
-                List<Compania> lstcompania = new List<Compania>();
-
-                using (NpgsqlConnection con = new NpgsqlConnection(connectionString))
-                {
-                    NpgsqlCommand cmd = new NpgsqlCommand(consulta, con);
-
-=======
         public IEnumerable<Compania> GetAllCompanias()
         {
             string cadena = "SELECT * FROM" + cod + "TAB_COMPANIA" + cod + "";
             try
             {
-                List<Compania> lstCompania = new List<Compania>();
+                List<Compania> lstcompania = new List<Compania>();
                 using (NpgsqlConnection con = new NpgsqlConnection(connectionString))
                 {
                     NpgsqlCommand cmd = new NpgsqlCommand(cadena, con);
->>>>>>> 66851b5e273fa8258515a35d3f2489ba07263c5e
+
                     con.Open();
                     NpgsqlDataReader rdr = cmd.ExecuteReader();
 
                     while (rdr.Read())
                     {
                         Compania compania = new Compania();
-<<<<<<< HEAD
+
                         compania.STR_IDCOMPANIA = rdr["STR_IDCOMPANIA"].ToString();
                         compania.STR_NOMBRE_COMPANIA = rdr["STR_NOMBRE_COMPANIA"].ToString();
                         compania.STR_ABREV_COMPANIA = rdr["STR_ABREV_COMPANIA"].ToString();
@@ -61,31 +47,6 @@ namespace AppGia.Controllers
                     con.Close();
                 }
                 return lstcompania;
-=======
-                        // id
-                        compania.STR_IDCOMPANIA = rdr["STR_IDCOMPANIA"].ToString();
-                        // nombre
-                        compania.STR_NOMBRE_COMPANIA = rdr["STR_NOMBRE_COMPANIA"].ToString();
-                        //abrev
-                        compania.STR_ABREV_COMPANIA = rdr["STR_ABREV_COMPANIA"].ToString();
-                        //etl
-                        compania.BOOL_ETL_COMPANIA = Convert.ToBoolean(rdr["BOOL_ETL_COMPANIA"]);
-                        //host
-                        compania.STR_HOST_COMPANIA = rdr["STR_HOST_COMPANIA"].ToString();
-                        //moneda
-                        compania.STR_MONEDA_COMPANIA = rdr["STR_MONEDA_COMPANIA"].ToString();
-
-                        //public string STR_USUARIO_ETL { get; set; }
-                        //public string STR_CONTRASENIA_ETL { get; set; }
-                        //public string STR_PUERTO_COMPANIA { get; set; }
-                        //public string STR_BD_COMPANIA { get; set; }
-
-                        lstCompania.Add(compania);
-                    }
-                    con.Close();
-                }
-
-                return lstCompania;
             }
             catch
             {
@@ -95,22 +56,24 @@ namespace AppGia.Controllers
 
         public int addCompania(Compania compania)
         {
-            string add = "INSERT INTO" + cod + "TAB_COMPANIA" + cod + "(" + cod + "STR_NOMBRE_COMPANIA" + cod + ") VALUES " +
-                "(@STR_NOMBRE_COMPANIA)";
+            string add = "INSERT INTO" + cod + "TAB_COMPANIA" + cod + "(" + cod + "STR_IDCOMPANIA" + cod +","+cod+ "STR_NOMBRE_COMPANIA"+cod+","+
+                cod+"STR_ABREV_COMPANIA"+cod+","+cod+"BOOL_ETL_COMPANIA"+cod+","+cod+"STR_HOST_COMPANIA"+cod+","+cod+"STR_PUERTO_COMPANIA"+cod+","+cod+"STR_USUARIO_ETL"+cod+
+                ","+cod+"STR_CONTRASENIA"+cod+","+cod+"STR_BD_COMPANIA"+cod+","+cod+"STR_MONEDA_COMPANIA"+cod+") VALUES " +
+                "(@STR_IDCOMPANIA,@STR_NOMBRE_COMPANIA,@STR_ABREV_COMPANIA,@BOOL_ETL_COMPANIA,@STR_HOST_COMPANIA,@STR_PUERTO_COMPANIA,@STR_USUARIO_ETL,@STR_CONTRASEÑA,@STR_BD_COMPANIA,@STR_MONEDA_COMPANIA)";
             try
             {
                 using (NpgsqlConnection con = new NpgsqlConnection(connectionString))
                 {
                     NpgsqlCommand cmd = new NpgsqlCommand(add, con);
                     //cmd.Parameters.AddWithValue("@INT_IDGRUPO", grupo.INT_IDGRUPO);
-                    cmd.Parameters.AddWithValue("@STR_NOMBRE_COMPANIA", compania.STR_NOMBRE_COMPANIA);
+                    cmd.Parameters.AddWithValue("@STR_IDCOMPANIA", compania.STR_IDCOMPANIA);
 
                     con.Open();
                     cmd.ExecuteNonQuery();
                     con.Close();
                 }
                 return 1;
->>>>>>> 66851b5e273fa8258515a35d3f2489ba07263c5e
+
             }
             catch
             {
