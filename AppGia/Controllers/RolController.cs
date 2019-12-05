90cdbd9bb5cdcb9cb90a7db2f101229640683aba
Freddy Seseña Dominguez <42950131+inzunsaNull@users.noreply.github.com>
2019-12-05 12:51:11 -0600

    Commit se agrego Controller y DataAccess de Rol

---
+++ AppGia/Controllers/RolController.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+using Microsoft.AspNetCore.Mvc;
+using AppGia.Models;
+
+namespace AppGia.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class RolController : ControllerBase
+    {
+        RolDataAccessLayer objRol = new RolDataAccessLayer();
+
+        // GET: api/Grupo
+        [HttpGet]
+        public IEnumerable<Rol> Get()
+        {
+            return objRol.GetAllRoles();
+        }
+
+        //// GET: api/Grupo/5
+        //[HttpGet("{id}", Name = "")]
+        //public string Get(int id)
+        //{
+        //    return "value";
+        //}
+
+        // POST: api/Grupo
+        [HttpPost]
+        public int Create([FromBody]Rol rol)
+        {
+            return objRol.addRol(rol);
+        }
+
+        //// PUT: api/Grupo/5
+        //[HttpPut("{id}")]
+        //public void Put(int id, [FromBody] string value)
+        //{
+        //}
+
+        //// DELETE: api/ApiWithActions/5
+        //[HttpDelete("{id}")]
+        //public void Delete(int id)
+        //{
+        //}
+    }
+}
---
+++ AppGia/Controllers/RolDataAccessLayer.cs
@@ -0,0 +1,69 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using AppGia.Models;
+using Npgsql;
+
+namespace AppGia.Controllers
+{
+    public class RolDataAccessLayer
+    {
+        private string connectionString = "User ID=postgres;Password=HolaMundo1;Host=192.168.1.73;Port=5432;Database=GIA;Pooling=true;";
+        char cod = '"';
+        public IEnumerable<Rol> GetAllRoles()
+        {
+            string cadena = "SELECT * FROM" + cod + "TAB_ROL" + cod + "";
+            try
+            {
+                List<Rol> lstrol = new List<Rol>();
+
+                using (NpgsqlConnection con = new NpgsqlConnection(connectionString))
+                {
+                    NpgsqlCommand cmd = new NpgsqlCommand(cadena, con);
+                    con.Open();
+                    NpgsqlDataReader rdr = cmd.ExecuteReader();
+
+                    while (rdr.Read())
+                    {
+                        Rol rol = new Rol();
+                        rol.STR_NOMBRE_ROL = rdr["STR_NOMBRE_ROL"].ToString();
+
+                        lstrol.Add(rol);
+                    }
+                    con.Close();
+                }
+
+                return lstrol;
+            }
+            catch
+            {
+                throw;
+            }
+        }
+
+        public int addRol(Rol rol)
+        {
+            string add = "INSERT INTO" + cod + "TAB_ROL" + cod + "(" + cod + "STR_NOMBRE_ROL" + cod + ") VALUES " +
+                "(@STR_NOMBRE_ROL)";
+            try
+            {
+                using (NpgsqlConnection con = new NpgsqlConnection(connectionString))
+                {
+                    NpgsqlCommand cmd = new NpgsqlCommand(add, con);
+                    //cmd.Parameters.AddWithValue("@INT_IDGRUPO", grupo.INT_IDGRUPO);
+                    cmd.Parameters.AddWithValue("@STR_NOMBRE_ROL", rol.STR_NOMBRE_ROL);
+
+                    con.Open();
+                    cmd.ExecuteNonQuery();
+                    con.Close();
+                }
+                return 1;
+            }
+            catch
+            {
+                throw;
+            }
+        }
+    }
+}
