1a744e8330e903bd877753fb23568e699ae3d44d
Juan Santillan <juan.santillan.guzman@gmail.com>
2020-01-17 17:12:59 -0600

    validacion de estado activo de usuario en el login

--- AppGia/Controllers/LoginDataAccessLayer.cs
+++ AppGia/Controllers/LoginDataAccessLayer.cs
@@ -24,12 +24,11 @@ namespace AppGia.Controllers
         public bool validacionLoginUsuario(Relacion relacion, Login lg)
         {
 
-
             string consulta = " select " + 1 + " from " + "(" + " select " + cod + "TAB_RELACION_USUARIO" + cod + "." + cod + "INT_IDGRUPO_P" + cod + "," + cod + "TAB_RELACION_USUARIO" + cod + "."
              + cod + "INT_IDUSUARIO_P" + cod + "," + cod + "TAB_USUARIO" + cod + "." + cod + "STR_NOMBRE_USUARIO" + cod + " from " + cod + "TAB_RELACION_USUARIO" + cod + " inner " + "  " + " join "
              + cod + "TAB_USUARIO" + cod + " on " + cod + "TAB_RELACION_USUARIO" + cod + "." + cod + "INT_IDUSUARIO_P" + cod + "=" + cod + "TAB_USUARIO"
              + cod + "." + cod + "INT_IDUSUARIO_P" + cod + " where " + cod + "TAB_USUARIO" + cod + "." + cod + "STR_USERNAME_USUARIO" + cod + " = " + "'" + lg.UserName + "'" + " and " 
-             + cod + "TAB_RELACION_USUARIO" + cod + "." + cod + "INT_IDGRUPO_P" + cod + " != " + 1 + ")" + "usuario";
+             + cod + "TAB_RELACION_USUARIO" + cod + "." + cod + "INT_IDGRUPO_P" + cod + " = " + relacion.INT_IDGRUPO_P + " and " +  cod + "BOOL_ESTATUS_LOGICO_USUARIO" + cod + " = "  + true +  ")" + "usuario";
 
             // string consulta = " SELECT " + 1 + " from " + cod + "TAB_RELACIONES" + cod + " WHERE " + cod + "INT_IDUSUARIO_F" + cod + " = " + relacion.INT_IDUSUARIO_F + " and " + cod + "INT_IDGRUPO_F" + cod + " = " + relacion.INT_IDGRUPO_F;
             try
