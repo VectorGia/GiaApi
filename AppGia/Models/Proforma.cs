62d06c6bd7d0c0ac1e7110dcf65ea4b857da664d
Jorge Quiroz <jmartinqe@hotmail.com>
2020-01-31 09:28:03 -0600

    Merge a Proforma.cs

--- AppGia/Models/Proforma.cs
+++ AppGia/Models/Proforma.cs
@@ -5,13 +5,8 @@ using System.Threading.Tasks;
 
 namespace AppGia.Models
 {
-<<<<<<< HEAD
 	public class Proforma
 	{
-=======
-    public class Proforma
-    {
->>>>>>> cefbfa55392a36a8adcf009d08f437f3caf8b8aa
 		public int id { get; set; }
 		public int anio { get; set; }
 		public int modelo_negocio_id { get; set; }
@@ -19,9 +14,6 @@ namespace AppGia.Models
 		public int periodo_id { get; set; }
 		public bool activo { get; set; }
 		public DateTime fecha_captura { get; set; }
-<<<<<<< HEAD
 
-=======
->>>>>>> cefbfa55392a36a8adcf009d08f437f3caf8b8aa
 	}
 }
