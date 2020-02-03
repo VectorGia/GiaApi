bfcb9435db375d384ba69bd329fd0c55b5309938
carlos <proyecto.giavector@gmail.com>
2020-02-03 15:45:01 -0600

    Cambios

--- AppGia/Models/Rubros.cs
+++ AppGia/Models/Rubros.cs
@@ -7,7 +7,6 @@ namespace AppGia.Models
 {
     public class Rubros
     {
-<<<<<<< HEAD
         public Int64 id { get; set; }
         public bool activo { get; set; }
         public string nombre { get; set; }
@@ -18,20 +17,6 @@ namespace AppGia.Models
         public string rangos_cuentas_incluidas { get; set; }
         public Int64 tipo_id { get; set; }
         public Int64 id_modelo_neg { get; set; }
-=======
-        public int id { set; get; }
-        public bool activo { set; get; }
-        public string nombre { set; get; }
-        public string nombreM { set; get; }
-        public string aritmetica { set; get; }
-        public string clave { set; get; }
-        public string naturaleza { set; get; }
-        public string rango_cuentas_excluidas { set; get; }
-        public string rangos_cuentas_incluidas { set; get; }
-        public int tipo_id { set; get; }
-        public int id_modelo_neg { set; get; }
->>>>>>> 7a9c2a771334c2e4c1032874b22ad707259b135c
-
 
     }
 }
