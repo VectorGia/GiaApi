fba2242419e1aebf719e3518a660ffb609f871cd
imartinez <imartinez@gmail.com>
2020-02-03 17:08:51 -0600

    Israel Martínez Cañedo Se agrega cambio de modelo de Balanza

--- AppGia/Controllers/BalanzaDataAccessLayer.cs
+++ AppGia/Controllers/BalanzaDataAccessLayer.cs
@@ -10,6 +10,7 @@ namespace AppGia.Controllers
     {
         NpgsqlConnection con;
         Conexion.Conexion conex = new Conexion.Conexion();
+
         public BalanzaDataAccessLayer()
         {
             con = conex.ConnexionDB();
@@ -25,7 +26,7 @@ namespace AppGia.Controllers
             "sum(" + cod + "DECI_MARCARGOS" + cod + ") as " + cod + "DECI_MARCARGOS" + cod + ", sum(" + cod + "DECI_MARABONOS" + cod + ") as " + cod + "DECI_MARABONOS" + cod + "," +
             "sum(" + cod + "DECI_ABRCARGOS" + cod + ") as " + cod + "DECI_ABRCARGOS" + cod + ", sum(" + cod + "DECI_ABRABONOS" + cod + ") as " + cod + "DECI_ABRABONOS" + cod + "," +
             "sum(" + cod + "DECI_MAYCARGOS" + cod + ") as " + cod + "DECI_MAYCARGOS" + cod + ", sum(" + cod + "DECI_MAYABONOS" + cod + ") as " + cod + "DECI_MAYABONOS" + cod + "," +
-            "sum(" + cod + "DECI_JUNCARGOS" + cod + ") as " + cod + "DECI_JUNCARGOS" + cod + ", sum (" + cod + "DECI_JUNABONOS" + cod + ") as " + cod + "DECI_JUNABONOS" + cod + "," +
+            "sum(" + cod + "DECI_JUNCARGOS" + cod + ") as " + cod + "DECI_JUNCARGOS" + cod + ", sum(" + cod + "DECI_JUNABONOS" + cod + ") as " + cod + "DECI_JUNABONOS" + cod + "," +
             "sum(" + cod + "DECI_JULCARGOS" + cod + ") as " + cod + "DECI_JULCARGOS" + cod + ", sum(" + cod + "DECI_JULABONOS" + cod + ") as " + cod + "DECI_JULABONOS" + cod + "," +
             "sum(" + cod + "DECI_AGOCARGOS" + cod + ") as " + cod + "DECI_AGOCARGOS" + cod + ", sum(" + cod + "DECI_AGOABONOS" + cod + ") as " + cod + "DECI_AGOABONOS" + cod + "," +
             "sum(" + cod + "DECI_SEPCARGOS" + cod + ") as " + cod + "DECI_SEPCARGOS" + cod + ", sum(" + cod + "DECI_SEPABONOS" + cod + ") as " + cod + "DECI_SEPABONOS" + cod + "," +
@@ -66,33 +67,33 @@ namespace AppGia.Controllers
                 NpgsqlDataReader rdr = cmd.ExecuteReader();
                 while (rdr.Read())
                 {
-                    balanza.INT_YEAR = Convert.ToInt32(rdr["INT_YEAR"]);
-                    balanza.TEXT_DESCRIPCION = rdr["STR_CONCEPTO"].ToString().Trim();
-                    balanza.DECI_SALINI = Convert.ToInt32(rdr["DECI_SALINI"]);
-                    balanza.DECI_ENECARGOS = Convert.ToInt32(rdr["DECI_ENECARGOS"]);
-                    balanza.DECI_ENEABONOS = Convert.ToInt32(rdr["DECI_ENEABONOS"]);
-                    balanza.DECI_FEBCARGOS = Convert.ToInt32(rdr["DECI_FEBCARGOS"]);
-                    balanza.DECI_FEBABONOS = Convert.ToInt32(rdr["DECI_FEBABONOS"]);
-                    balanza.DECI_MARCARGOS = Convert.ToInt32(rdr["DECI_MARCARGOS"]);
-                    balanza.DECI_MARABONOS = Convert.ToInt32(rdr["DECI_MARABONOS"]);
-                    balanza.DECI_ABRCARGOS = Convert.ToInt32(rdr["DECI_ABRCARGOS"]);
-                    balanza.DECI_ABRABONOS = Convert.ToInt32(rdr["DECI_ABRABONOS"]);
-                    balanza.DECI_MAYCARGOS = Convert.ToInt32(rdr["DECI_MAYCARGOS"]);
-                    balanza.DECI_MAYABONOS = Convert.ToInt32(rdr["DECI_MAYABONOS"]);
-                    balanza.DECI_JUNCARGOS = Convert.ToInt32(rdr["DECI_JUNCARGOS"]);
-                    balanza.DECI_JUNABONOS = Convert.ToInt32(rdr["DECI_JUNABONOS"]);
-                    balanza.DECI_JULCARGOS = Convert.ToInt32(rdr["DECI_JULCARGOS"]);
-                    balanza.DECI_JULABONOS = Convert.ToInt32(rdr["DECI_JULABONOS"]);
-                    balanza.DECI_AGOCARGOS = Convert.ToInt32(rdr["DECI_AGOCARGOS"]);
-                    balanza.DECI_AGOABONOS = Convert.ToInt32(rdr["DECI_AGOABONOS"]);
-                    balanza.DECI_SEPCARGOS = Convert.ToInt32(rdr["DECI_SEPCARGOS"]);
-                    balanza.DECI_SEPABONOS = Convert.ToInt32(rdr["DECI_SEPABONOS"]);
-                    balanza.DECI_OCTCARGOS = Convert.ToInt32(rdr["DECI_OCTCARGOS"]);
-                    balanza.DECI_OCTABONOS = Convert.ToInt32(rdr["DECI_OCTABONOS"]);
-                    balanza.DECI_NOVCARGOS = Convert.ToInt32(rdr["DECI_NOVCARGOS"]);
-                    balanza.DECI_NOVABONOS = Convert.ToInt32(rdr["DECI_NOVABONOS"]);
-                    balanza.DECI_DICCARGOS = Convert.ToInt32(rdr["DECI_DICCARGOS"]);
-                    balanza.DECI_DICABONOS = Convert.ToInt32(rdr["DECI_DICABONOS"]);
+                    balanza.year = Convert.ToInt32(rdr["INT_YEAR"]);
+                    balanza.concepto = rdr["STR_CONCEPTO"].ToString().Trim();
+                    balanza.salini = Convert.ToInt32(rdr["DECI_SALINI"]);
+                    balanza.enecargos = Convert.ToInt32(rdr["DECI_ENECARGOS"]);
+                    balanza.eneabonos = Convert.ToInt32(rdr["DECI_ENEABONOS"]);
+                    balanza.febcargos = Convert.ToInt32(rdr["DECI_FEBCARGOS"]);
+                    balanza.febabonos = Convert.ToInt32(rdr["DECI_FEBABONOS"]);
+                    balanza.marcargos = Convert.ToInt32(rdr["DECI_MARCARGOS"]);
+                    balanza.marabonos = Convert.ToInt32(rdr["DECI_MARABONOS"]);
+                    balanza.abrcargos = Convert.ToInt32(rdr["DECI_ABRCARGOS"]);
+                    balanza.abrabonos = Convert.ToInt32(rdr["DECI_ABRABONOS"]);
+                    balanza.maycargos = Convert.ToInt32(rdr["DECI_MAYCARGOS"]);
+                    balanza.mayabonos = Convert.ToInt32(rdr["DECI_MAYABONOS"]);
+                    balanza.juncargos = Convert.ToInt32(rdr["DECI_JUNCARGOS"]);
+                    balanza.junabonos = Convert.ToInt32(rdr["DECI_JUNABONOS"]);
+                    balanza.julcargos = Convert.ToInt32(rdr["DECI_JULCARGOS"]);
+                    balanza.julabonos = Convert.ToInt32(rdr["DECI_JULABONOS"]);
+                    balanza.agocargos = Convert.ToInt32(rdr["DECI_AGOCARGOS"]);
+                    balanza.agoabonos = Convert.ToInt32(rdr["DECI_AGOABONOS"]);
+                    balanza.sepcargos = Convert.ToInt32(rdr["DECI_SEPCARGOS"]);
+                    balanza.sepabonos = Convert.ToInt32(rdr["DECI_SEPABONOS"]);
+                    balanza.octcargos = Convert.ToInt32(rdr["DECI_OCTCARGOS"]);
+                    balanza.octabonos = Convert.ToInt32(rdr["DECI_OCTABONOS"]);
+                    balanza.novcargos = Convert.ToInt32(rdr["DECI_NOVCARGOS"]);
+                    balanza.novabonos = Convert.ToInt32(rdr["DECI_NOVABONOS"]);
+                    balanza.diccargos = Convert.ToInt32(rdr["DECI_DICCARGOS"]);
+                    balanza.dicabonos = Convert.ToInt32(rdr["DECI_DICABONOS"]);
 
                 }
 
@@ -204,48 +205,47 @@ namespace AppGia.Controllers
 
                     NpgsqlCommand cmd = new NpgsqlCommand(addBalanza, con);
                     //cmd.Parameters.AddWithValue("@INT_IDBALANZA", NpgsqlTypes.NpgsqlDbType.Integer, balanza.INT_IDBALANZA);
-                    cmd.Parameters.AddWithValue("@TEXT_CTA", NpgsqlTypes.NpgsqlDbType.Text, balanza.TEXT_CTA.Trim());
-                    cmd.Parameters.AddWithValue("@TEXT_SCTA", NpgsqlTypes.NpgsqlDbType.Text, balanza.TEXT_SCTA.Trim());
-                    cmd.Parameters.AddWithValue("@TEXT_SSCTA", NpgsqlTypes.NpgsqlDbType.Text, balanza.TEXT_SSCTA.Trim());
-                    cmd.Parameters.AddWithValue("@INT_YEAR", NpgsqlTypes.NpgsqlDbType.Integer, balanza.INT_YEAR);
-                    cmd.Parameters.AddWithValue("@DECI_SALINI", NpgsqlTypes.NpgsqlDbType.Double, balanza.DECI_SALINI);
-                    cmd.Parameters.AddWithValue("@DECI_ENECARGOS", NpgsqlTypes.NpgsqlDbType.Double, balanza.DECI_ENECARGOS);
-                    cmd.Parameters.AddWithValue("@DECI_ENEABONOS", NpgsqlTypes.NpgsqlDbType.Double, balanza.DECI_ENEABONOS);
-                    cmd.Parameters.AddWithValue("@DECI_FEBCARGOS", NpgsqlTypes.NpgsqlDbType.Double, balanza.DECI_FEBCARGOS);
-                    cmd.Parameters.AddWithValue("@DECI_FEBABONOS", NpgsqlTypes.NpgsqlDbType.Double, balanza.DECI_FEBABONOS);
-                    cmd.Parameters.AddWithValue("@DECI_MARCARGOS", NpgsqlTypes.NpgsqlDbType.Double, balanza.DECI_MARCARGOS);
-                    cmd.Parameters.AddWithValue("@DECI_MARABONOS", NpgsqlTypes.NpgsqlDbType.Double, balanza.DECI_MARABONOS);
-                    cmd.Parameters.AddWithValue("@DECI_ABRCARGOS", NpgsqlTypes.NpgsqlDbType.Double, balanza.DECI_ABRCARGOS);
-                    cmd.Parameters.AddWithValue("@DECI_ABRABONOS", NpgsqlTypes.NpgsqlDbType.Double, balanza.DECI_ABRABONOS);
-                    cmd.Parameters.AddWithValue("@DECI_MAYCARGOS", NpgsqlTypes.NpgsqlDbType.Double, balanza.DECI_MAYCARGOS);
-                    cmd.Parameters.AddWithValue("@DECI_MAYABONOS", NpgsqlTypes.NpgsqlDbType.Double, balanza.DECI_MAYABONOS);
-                    cmd.Parameters.AddWithValue("@DECI_JUNCARGOS", NpgsqlTypes.NpgsqlDbType.Double, balanza.DECI_JUNCARGOS);
-                    cmd.Parameters.AddWithValue("@DECI_JUNABONOS", NpgsqlTypes.NpgsqlDbType.Double, balanza.DECI_JUNABONOS);
-                    cmd.Parameters.AddWithValue("@DECI_JULCARGOS", NpgsqlTypes.NpgsqlDbType.Double, balanza.DECI_JULCARGOS);
-                    cmd.Parameters.AddWithValue("@DECI_JULABONOS", NpgsqlTypes.NpgsqlDbType.Double, balanza.DECI_JULABONOS);
-                    cmd.Parameters.AddWithValue("@DECI_AGOCARGOS", NpgsqlTypes.NpgsqlDbType.Double, balanza.DECI_AGOCARGOS);
-                    cmd.Parameters.AddWithValue("@DECI_AGOABONOS", NpgsqlTypes.NpgsqlDbType.Double, balanza.DECI_AGOABONOS);
-                    cmd.Parameters.AddWithValue("@DECI_SEPCARGOS", NpgsqlTypes.NpgsqlDbType.Double, balanza.DECI_SEPCARGOS);
-                    cmd.Parameters.AddWithValue("@DECI_SEPABONOS", NpgsqlTypes.NpgsqlDbType.Double, balanza.DECI_SEPABONOS);
-                    cmd.Parameters.AddWithValue("@DECI_OCTCARGOS", NpgsqlTypes.NpgsqlDbType.Double, balanza.DECI_OCTCARGOS);
-                    cmd.Parameters.AddWithValue("@DECI_OCTABONOS", NpgsqlTypes.NpgsqlDbType.Double, balanza.DECI_OCTABONOS);
-                    cmd.Parameters.AddWithValue("@DECI_NOVCARGOS", NpgsqlTypes.NpgsqlDbType.Double, balanza.DECI_NOVCARGOS);
-                    cmd.Parameters.AddWithValue("@DECI_NOVABONOS", NpgsqlTypes.NpgsqlDbType.Double, balanza.DECI_NOVABONOS);
-                    cmd.Parameters.AddWithValue("@DECI_DICCARGOS", NpgsqlTypes.NpgsqlDbType.Double, balanza.DECI_DICCARGOS);
-                    cmd.Parameters.AddWithValue("@DECI_DICABONOS", NpgsqlTypes.NpgsqlDbType.Double, balanza.DECI_DICABONOS);
-                    //cmd.Parameters.AddWithValue("@INT_CC", NpgsqlTypes.NpgsqlDbType.Integer, balanza.INT_CC);
-                    //cmd.Parameters.AddWithValue("@TEXT_DESCRIPCION", NpgsqlTypes.NpgsqlDbType.Text, balanza.TEXT_DESCRIPCION.Trim());
-                    //cmd.Parameters.AddWithValue("@TEXT_DESCRIPCION2", NpgsqlTypes.NpgsqlDbType.Text, balanza.TEXT_DESCRIPCION2.Trim());
-                    cmd.Parameters.AddWithValue("@INT_INCLUIR_SUMA", NpgsqlTypes.NpgsqlDbType.Integer, balanza.INT_INCLUIR_SUMA);
-                    cmd.Parameters.AddWithValue("@INT_TIPO_EXTRACCION", NpgsqlTypes.NpgsqlDbType.Integer, balanza.INT_TIPO_EXTRACCION);
-                    cmd.Parameters.AddWithValue("@TEXT_FECH_EXTR", NpgsqlTypes.NpgsqlDbType.Text, balanza.TEXT_FECH_EXTR.Trim());
-                    cmd.Parameters.AddWithValue("@TEXT_HORA", NpgsqlTypes.NpgsqlDbType.Text, balanza.TEXT_HORA.Trim());
-                    cmd.Parameters.AddWithValue("@INT_ID_EMPRESA", NpgsqlTypes.NpgsqlDbType.Integer, balanza.INT_ID_EMPRESA);
-                    cmd.Parameters.AddWithValue("@DECI_CIERRE_CARGOS", NpgsqlTypes.NpgsqlDbType.Double, balanza.DECI_CIERRE_CARGOS);
-                    cmd.Parameters.AddWithValue("@DECI_CIERRE_ABONOS", NpgsqlTypes.NpgsqlDbType.Double, balanza.DECI_CIERRE_ABONOS);
-                    cmd.Parameters.AddWithValue("@INT_ACTA", NpgsqlTypes.NpgsqlDbType.Integer, balanza.INT_ACTA);
-                    cmd.Parameters.AddWithValue("@TEXT_CC", NpgsqlTypes.NpgsqlDbType.Text, balanza.TEXT_CC);
-
+                    cmd.Parameters.AddWithValue("@CTA", NpgsqlTypes.NpgsqlDbType.Text, balanza.cta.Trim());
+                    cmd.Parameters.AddWithValue("@SCTA", NpgsqlTypes.NpgsqlDbType.Text, balanza.scta.Trim());
+                    cmd.Parameters.AddWithValue("@SSCTA", NpgsqlTypes.NpgsqlDbType.Text, balanza.sscta.Trim());
+                    cmd.Parameters.AddWithValue("@YEAR", NpgsqlTypes.NpgsqlDbType.Integer, balanza.year);
+                    cmd.Parameters.AddWithValue("@SALINI", NpgsqlTypes.NpgsqlDbType.Double, balanza.salini);
+                    cmd.Parameters.AddWithValue("@ENECARGOS", NpgsqlTypes.NpgsqlDbType.Double, balanza.enecargos);
+                    cmd.Parameters.AddWithValue("@ENEABONOS", NpgsqlTypes.NpgsqlDbType.Double, balanza.eneabonos);
+                    cmd.Parameters.AddWithValue("@FEBCARGOS", NpgsqlTypes.NpgsqlDbType.Double, balanza.febcargos);
+                    cmd.Parameters.AddWithValue("@FEBABONOS", NpgsqlTypes.NpgsqlDbType.Double, balanza.febabonos);
+                    cmd.Parameters.AddWithValue("@MARCARGOS", NpgsqlTypes.NpgsqlDbType.Double, balanza.marcargos);
+                    cmd.Parameters.AddWithValue("@MARABONOS", NpgsqlTypes.NpgsqlDbType.Double, balanza.marabonos);
+                    cmd.Parameters.AddWithValue("@ABRCARGOS", NpgsqlTypes.NpgsqlDbType.Double, balanza.abrcargos);
+                    cmd.Parameters.AddWithValue("@ABRABONOS", NpgsqlTypes.NpgsqlDbType.Double, balanza.abrabonos);
+                    cmd.Parameters.AddWithValue("@MAYCARGOS", NpgsqlTypes.NpgsqlDbType.Double, balanza.maycargos);
+                    cmd.Parameters.AddWithValue("@MAYABONOS", NpgsqlTypes.NpgsqlDbType.Double, balanza.mayabonos);
+                    cmd.Parameters.AddWithValue("@JUNCARGOS", NpgsqlTypes.NpgsqlDbType.Double, balanza.juncargos);
+                    cmd.Parameters.AddWithValue("@JUNABONOS", NpgsqlTypes.NpgsqlDbType.Double, balanza.junabonos);
+                    cmd.Parameters.AddWithValue("@JULCARGOS", NpgsqlTypes.NpgsqlDbType.Double, balanza.julcargos);
+                    cmd.Parameters.AddWithValue("@JULABONOS", NpgsqlTypes.NpgsqlDbType.Double, balanza.julabonos);
+                    cmd.Parameters.AddWithValue("@AGOCARGOS", NpgsqlTypes.NpgsqlDbType.Double, balanza.agocargos);
+                    cmd.Parameters.AddWithValue("@AGOABONOS", NpgsqlTypes.NpgsqlDbType.Double, balanza.agoabonos);
+                    cmd.Parameters.AddWithValue("@SEPCARGOS", NpgsqlTypes.NpgsqlDbType.Double, balanza.sepcargos);
+                    cmd.Parameters.AddWithValue("@SEPABONOS", NpgsqlTypes.NpgsqlDbType.Double, balanza.sepabonos);
+                    cmd.Parameters.AddWithValue("@OCTCARGOS", NpgsqlTypes.NpgsqlDbType.Double, balanza.octcargos);
+                    cmd.Parameters.AddWithValue("@OCTABONOS", NpgsqlTypes.NpgsqlDbType.Double, balanza.octabonos);
+                    cmd.Parameters.AddWithValue("@NOVCARGOS", NpgsqlTypes.NpgsqlDbType.Double, balanza.novcargos);
+                    cmd.Parameters.AddWithValue("@NOVABONOS", NpgsqlTypes.NpgsqlDbType.Double, balanza.novabonos);
+                    cmd.Parameters.AddWithValue("@DICCARGOS", NpgsqlTypes.NpgsqlDbType.Double, balanza.diccargos);
+                    cmd.Parameters.AddWithValue("@DICABONOS", NpgsqlTypes.NpgsqlDbType.Double, balanza.dicabonos);
+                    //cmd.Parameters.AddWithValue("@CC", NpgsqlTypes.NpgsqlDbType.Integer, balanza.CC);
+                    //cmd.Parameters.AddWithValue("@DESCRIPCION", NpgsqlTypes.NpgsqlDbType.Text, balanza.DESCRIPCION.Trim());
+                    //cmd.Parameters.AddWithValue("@DESCRIPCION2", NpgsqlTypes.NpgsqlDbType.Text, balanza.DESCRIPCION2.Trim());
+                    cmd.Parameters.AddWithValue("@INCLUIR_SUMA", NpgsqlTypes.NpgsqlDbType.Integer, balanza.incluir_suma);
+                    cmd.Parameters.AddWithValue("@TIPO_EXTRACCION", NpgsqlTypes.NpgsqlDbType.Integer, balanza.tipo_extraccion);
+                    cmd.Parameters.AddWithValue("@FECH_EXTR", NpgsqlTypes.NpgsqlDbType.Text, balanza.fecha_carga.Trim());
+                    cmd.Parameters.AddWithValue("@HORA", NpgsqlTypes.NpgsqlDbType.Text, balanza.hora_carga.Trim());
+                    cmd.Parameters.AddWithValue("@ID_EMPRESA", NpgsqlTypes.NpgsqlDbType.Integer, balanza.id_empresa);
+                    cmd.Parameters.AddWithValue("@CIERRE_CARGOS", NpgsqlTypes.NpgsqlDbType.Double, balanza.cierre_cargos);
+                    cmd.Parameters.AddWithValue("@CIERRE_ABONOS", NpgsqlTypes.NpgsqlDbType.Double, balanza.cierre_abonos);
+                    cmd.Parameters.AddWithValue("@ACTA", NpgsqlTypes.NpgsqlDbType.Integer, balanza.acta);
+                    cmd.Parameters.AddWithValue("@CC", NpgsqlTypes.NpgsqlDbType.Text, balanza.cc);
 
                     con.Open();
                     int cantFilaAfect = Convert.ToInt32(cmd.ExecuteNonQuery());
