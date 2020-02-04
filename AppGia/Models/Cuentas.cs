bd9141381b913749668442210b7d2897e49bb472
imartinez <imartinez@gmail.com>
2020-02-03 20:07:55 -0600

    Israel Martinez Cañedo Integracion de cambios de modelo

--- AppGia/Models/Cuentas.cs
+++ AppGia/Models/Cuentas.cs
@@ -6,12 +6,19 @@ using System.Threading.Tasks;
 namespace AppGia.Models
 {
     public class Cuentas
-    {
-        public int INT_ID_CUENTAS { get; set; }
-        public string CHAR_CTA { get; set; }
-        public string CHAR_SUB_CTA { get; set; }
-        public string CHAR_SUB_SUB_CTA { get; set; }
-        public string TEXT_DESCRIPCION { get; set; }
-        public int INT_ID_COMPANIA_F { get; set; }
+    {        //public int INT_ID_CUENTAS { get; set; }
+        //public string CHAR_CTA { get; set; }
+        //public string CHAR_SUB_CTA { get; set; }
+        //public string CHAR_SUB_SUB_CTA { get; set; }
+        //public string TEXT_DESCRIPCION { get; set; }
+        //public int INT_ID_COMPANIA_F { get; set; }
+
+        public Int64 id { get; set; }
+        public Boolean activo { get; set; }
+        public string cta { get; set; }
+        public string descripcion { get; set; }
+        public int id_companiaf { get; set; }
+        public string sub_cta { get; set; }
+        public string sub_sub_cta { get; set; }
     }
 }
