625cac1ae3b16ed552bcfeb9ab8d13e8db6acbdb
imartinez <imartinez@gmail.com>
2020-02-03 20:11:18 -0600

    Israel Martinez Cañedo Integracion de cambios de modelo

---
+++ AppGia/Models/Tipo_Proforma.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace AppGia.Models
+{
+    public class Tipo_Proforma
+    {
+        public Int64 id { get; set; }
+        public Boolean activo { get; set; }
+        public string nombre { get; set; }
+        public string clave { get; set; }
+        public string descripcion { get; set; }
+        public string fec_modif { get; set; }
+        public Int64 idusuario { get; set; }
+        public int mes_inicio { get; set; }
+    }
+}
