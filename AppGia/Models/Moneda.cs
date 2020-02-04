f5929ff1b119ace30741d3bb7af9b349206223ba
imartinez <imartinez@gmail.com>
2020-02-03 20:04:15 -0600

    Israel Martinez Cañedo Integracion de cambios de modelo

--- AppGia/Models/Moneda.cs
+++ AppGia/Models/Moneda.cs
@@ -7,14 +7,14 @@ namespace AppGia.Models
 {
     public class Moneda
     {
-    
-        public int id { get; set; }
-        public bool activo { get; set; }
+
+        public Int64 id { get; set; }
+        public Boolean activo { get; set; }
         public string clave { get; set; }
         public string descripcion { get; set; }
         public string pais { get; set; }
         public string simbolo { get; set; }
-        public bool estatus { get; set; }
+     
 
     }
 }
