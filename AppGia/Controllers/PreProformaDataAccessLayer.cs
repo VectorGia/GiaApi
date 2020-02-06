01d5b437facd3892a4e5c96199fd8fddbd7a58ec
carlos <proyecto.giavector@gmail.com>
2020-02-06 12:07:39 -0600

    Cambios a la PreProforma

--- AppGia/Controllers/PreProformaDataAccessLayer.cs
+++ AppGia/Controllers/PreProformaDataAccessLayer.cs
@@ -22,7 +22,7 @@ namespace AppGia.Controllers
             con = conex.ConnexionDB();
         }
 
-        public int MontosConsolidados()
+        public int MontosConsolidadosContable()
         {
             
             GeneraQry qry = new GeneraQry();
@@ -70,47 +70,48 @@ namespace AppGia.Controllers
                             else
                             {
                                 cambiol = 1;
+                                cambiop = 1;
                             }
                             foreach (DataRow rub in dr.Rows)
                             {
                                 MontosConsolidados montos = new MontosConsolidados();
                                 montos.activo = true;
-                                montos.enero_abono_resultado = ((Convert.ToDouble(rub["eneabonos"]) * cambiol) * cambiop);
-                                montos.enero_cargo_resultado = ((Convert.ToDouble(rub["enecargos"]) * cambiol) * cambiop);
-                                montos.enero_total_resultado = ((Convert.ToDouble(rub["enetotal"]) * cambiol) * cambiop);
-                                montos.febrero_abono_resultado = ((Convert.ToDouble(rub["febabonos"]) * cambiol) * cambiop);
-                                montos.febrero_cargo_resultado = ((Convert.ToDouble(rub["febcargos"]) * cambiol) * cambiop);
-                                montos.febrero_total_resultado = ((Convert.ToDouble(rub["febtotal"]) * cambiol) * cambiop);
-                                montos.marzo_abono_resultado = ((Convert.ToDouble(rub["marabonos"]) * cambiol) * cambiop);
-                                montos.marzo_cargo_resultado = ((Convert.ToDouble(rub["marcargos"]) * cambiol) * cambiop);
-                                montos.marzo_total_resultado = ((Convert.ToDouble(rub["martotal"]) * cambiol) * cambiop);
-                                montos.abril_abono_resultado = ((Convert.ToDouble(rub["abrabonos"]) * cambiol) * cambiop);
-                                montos.abril_cargo_resultado = ((Convert.ToDouble(rub["abrcargos"]) * cambiol) * cambiop);
-                                montos.abril_total_resultado = ((Convert.ToDouble(rub["abrtotal"]) * cambiol) * cambiop);
-                                montos.mayo_abono_resultado = ((Convert.ToDouble(rub["mayabonos"]) * cambiol) * cambiop);
-                                montos.mayo_cargo_resultado = ((Convert.ToDouble(rub["maycargos"]) * cambiol) * cambiop);
-                                montos.mayo_total_resultado = ((Convert.ToDouble(rub["maytotal"]) * cambiol) * cambiop);
-                                montos.junio_abono_resultado = ((Convert.ToDouble(rub["junabonos"]) * cambiol) * cambiop);
-                                montos.junio_cargo_resultado = ((Convert.ToDouble(rub["juncargos"]) * cambiol) * cambiop);
-                                montos.junio_total_resultado = ((Convert.ToDouble(rub["juntotal"]) * cambiol) * cambiop);
-                                montos.julio_abono_resultado = ((Convert.ToDouble(rub["julabonos"]) * cambiol) * cambiop);
-                                montos.julio_cargo_resultado = ((Convert.ToDouble(rub["julcargos"]) * cambiol) * cambiop);
-                                montos.julio_total_resultado = ((Convert.ToDouble(rub["jultotal"]) * cambiol) * cambiop);
-                                montos.agosto_abono_resultado = ((Convert.ToDouble(rub["agoabonos"]) * cambiol) * cambiop);
-                                montos.agosto_cargo_resultado = ((Convert.ToDouble(rub["agocargos"]) * cambiol) * cambiop);
-                                montos.agosto_total_resultado = ((Convert.ToDouble(rub["agototal"]) * cambiol) * cambiop);
-                                montos.septiembre_abono_resultado = ((Convert.ToDouble(rub["sepabonos"]) * cambiol) * cambiop);
-                                montos.septiembre_cargo_resultado = ((Convert.ToDouble(rub["sepcargos"]) * cambiol) * cambiop);
-                                montos.septiembre_total_resultado = ((Convert.ToDouble(rub["septotal"]) * cambiol) * cambiop);
-                                montos.octubre_abono_resultado = ((Convert.ToDouble(rub["octabonos"]) * cambiol) * cambiop);
-                                montos.octubre_cargo_resultado = ((Convert.ToDouble(rub["octcargos"]) * cambiol) * cambiop);
-                                montos.octubre_total_resultado = ((Convert.ToDouble(rub["octtotal"]) * cambiol) * cambiop);
-                                montos.noviembre_abono_resultado = ((Convert.ToDouble(rub["novabonos"]) * cambiol) * cambiop);
-                                montos.noviembre_cargo_resultado = ((Convert.ToDouble(rub["novcargos"]) * cambiol) * cambiop);
-                                montos.noviembre_total_resultado = ((Convert.ToDouble(rub["novtotal"]) * cambiol) * cambiop);
-                                montos.diciembre_abono_resultado = ((Convert.ToDouble(rub["dicabonos"]) * cambiol) * cambiop);
-                                montos.diciembre_cargo_resultado = ((Convert.ToDouble(rub["diccargos"]) * cambiol) * cambiop);
-                                montos.diciembre_total_resultado = ((Convert.ToDouble(rub["dictotal"]) * cambiol) * cambiop);
+                                montos.enero_abono_resultado = ((Convert.ToDouble(rub["eneabonos"]) * cambiol) / cambiop);
+                                montos.enero_cargo_resultado = ((Convert.ToDouble(rub["enecargos"]) * cambiol) / cambiop);
+                                montos.enero_total_resultado = ((Convert.ToDouble(rub["enetotal"]) * cambiol) / cambiop);
+                                montos.febrero_abono_resultado = ((Convert.ToDouble(rub["febabonos"]) * cambiol) / cambiop);
+                                montos.febrero_cargo_resultado = ((Convert.ToDouble(rub["febcargos"]) * cambiol) / cambiop);
+                                montos.febrero_total_resultado = ((Convert.ToDouble(rub["febtotal"]) * cambiol) / cambiop);
+                                montos.marzo_abono_resultado = ((Convert.ToDouble(rub["marabonos"]) * cambiol) / cambiop);
+                                montos.marzo_cargo_resultado = ((Convert.ToDouble(rub["marcargos"]) * cambiol) / cambiop);
+                                montos.marzo_total_resultado = ((Convert.ToDouble(rub["martotal"]) * cambiol) / cambiop);
+                                montos.abril_abono_resultado = ((Convert.ToDouble(rub["abrabonos"]) * cambiol) / cambiop);
+                                montos.abril_cargo_resultado = ((Convert.ToDouble(rub["abrcargos"]) * cambiol) / cambiop);
+                                montos.abril_total_resultado = ((Convert.ToDouble(rub["abrtotal"]) * cambiol) / cambiop);
+                                montos.mayo_abono_resultado = ((Convert.ToDouble(rub["mayabonos"]) * cambiol) / cambiop);
+                                montos.mayo_cargo_resultado = ((Convert.ToDouble(rub["maycargos"]) * cambiol) / cambiop);
+                                montos.mayo_total_resultado = ((Convert.ToDouble(rub["maytotal"]) * cambiol) / cambiop);
+                                montos.junio_abono_resultado = ((Convert.ToDouble(rub["junabonos"]) * cambiol) / cambiop);
+                                montos.junio_cargo_resultado = ((Convert.ToDouble(rub["juncargos"]) * cambiol) / cambiop);
+                                montos.junio_total_resultado = ((Convert.ToDouble(rub["juntotal"]) * cambiol) / cambiop);
+                                montos.julio_abono_resultado = ((Convert.ToDouble(rub["julabonos"]) * cambiol) / cambiop);
+                                montos.julio_cargo_resultado = ((Convert.ToDouble(rub["julcargos"]) * cambiol) / cambiop);
+                                montos.julio_total_resultado = ((Convert.ToDouble(rub["jultotal"]) * cambiol) / cambiop);
+                                montos.agosto_abono_resultado = ((Convert.ToDouble(rub["agoabonos"]) * cambiol) / cambiop);
+                                montos.agosto_cargo_resultado = ((Convert.ToDouble(rub["agocargos"]) * cambiol) / cambiop);
+                                montos.agosto_total_resultado = ((Convert.ToDouble(rub["agototal"]) * cambiol) / cambiop);
+                                montos.septiembre_abono_resultado = ((Convert.ToDouble(rub["sepabonos"]) * cambiol) / cambiop);
+                                montos.septiembre_cargo_resultado = ((Convert.ToDouble(rub["sepcargos"]) * cambiol) / cambiop);
+                                montos.septiembre_total_resultado = ((Convert.ToDouble(rub["septotal"]) * cambiol) / cambiop);
+                                montos.octubre_abono_resultado = ((Convert.ToDouble(rub["octabonos"]) * cambiol) / cambiop);
+                                montos.octubre_cargo_resultado = ((Convert.ToDouble(rub["octcargos"]) * cambiol) / cambiop);
+                                montos.octubre_total_resultado = ((Convert.ToDouble(rub["octtotal"]) * cambiol) / cambiop);
+                                montos.noviembre_abono_resultado = ((Convert.ToDouble(rub["novabonos"]) * cambiol) / cambiop);
+                                montos.noviembre_cargo_resultado = ((Convert.ToDouble(rub["novcargos"]) * cambiol) / cambiop);
+                                montos.noviembre_total_resultado = ((Convert.ToDouble(rub["novtotal"]) * cambiol) / cambiop);
+                                montos.diciembre_abono_resultado = ((Convert.ToDouble(rub["dicabonos"]) * cambiol) / cambiop);
+                                montos.diciembre_cargo_resultado = ((Convert.ToDouble(rub["diccargos"]) * cambiol) / cambiop);
+                                montos.diciembre_total_resultado = ((Convert.ToDouble(rub["dictotal"]) * cambiol) / cambiop);
                                 montos.anio = fechaactual.Year;
                                 montos.fecha = fechaactual;
                                 montos.mes = fechaactual.Month;
