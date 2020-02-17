bf4b45ea55444cd93038fd226b29cca85a4a994f
imartinez <imartinez@gmail.com>
2020-02-17 10:52:29 -0600

    .

---
+++ AppGia/Controllers/ActualizaProformaController.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using AppGia.Models;
+
+namespace AppGia.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class ActualizaProformaController : ControllerBase
+    {
+        ProformaDataAccessLayer objProforma = new ProformaDataAccessLayer();
+        ProformaDetalleDataAccessLayer objProformaDetalle = new ProformaDetalleDataAccessLayer();
+
+        // GET: api/ActualizaProforma
+        [HttpGet]
+        public IEnumerable<string> Get()
+        {
+            return new string[] { "value1", "value2" };
+        }
+
+        // GET: api/ActualizaProforma/5
+        [HttpGet("{id}", Name = "Get")]
+        public string Get(int id)
+        {
+            return "value";
+        }
+
+        // POST: api/ActualizaProforma
+        [HttpPost]
+        public void Post([FromBody] string value)
+        {
+        }
+
+        // PUT: api/ActualizaProforma/5
+        [HttpPut("{id}")]
+        public int Put(int id, [FromBody] List<ProformaDetalle> profDetalle)
+        {
+            return objProforma.ActualizaProforma(profDetalle);
+        }
+
+        // DELETE: api/ApiWithActions/5
+        [HttpDelete("{id}")]
+        public void Delete(int id)
+        {
+        }
+    }
+}
