44974a11b0e13d4085bee2bbb47f872ca1cab65b
Jorge Quiroz <jmartinqe@hotmail.com>
2020-02-20 13:37:47 -0600

    JMQE Se agregan modelo, controlador y DataAccess para el catalogo de Unidades de Negocio

---
+++ AppGia/Controllers/UnidadNegocioController.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using AppGia.Models;
+
+namespace AppGia.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class UnidadNegocioController : ControllerBase
+    {
+        UnidadNegocioDataAccessLayer objUnidadNeg = new UnidadNegocioDataAccessLayer();
+        // GET: api/UnidadNegocio
+        [HttpGet]
+        public List<UnidadNegocio> Get()
+        {
+            return objUnidadNeg.GetAllUnidadNegocio();
+        }
+
+        // GET: api/UnidadNegocio/5
+        [HttpGet("{id}", Name = "GetUnidadNegocio")]
+        public UnidadNegocio GetUnidadNegocio(int idUnidadNegocio)
+        {
+            return objUnidadNeg.GetUnidadNegocio(idUnidadNegocio);
+        }
+
+        // POST: api/UnidadNegocio
+        [HttpPost]
+        public int Post([FromBody] UnidadNegocio unidadNegocio)
+        {
+            return objUnidadNeg.UpdateUnidadNegocio(unidadNegocio);
+        }
+
+        // PUT: api/UnidadNegocio/5
+        [HttpPut("{id}")]
+        public int Put([FromBody] UnidadNegocio unidadNegocio)
+        {
+            return objUnidadNeg.AddUnidadNegocio(unidadNegocio);
+        }
+
+        // DELETE: api/ApiWithActions/5
+        [HttpDelete("{id}")]
+        public void Delete(int id)
+        {
+        }
+    }
+}
