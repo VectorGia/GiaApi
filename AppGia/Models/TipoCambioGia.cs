e247ef9e9e3aac0882ee19cc1f3d6519b54ae6dd
eLeasing <eleasingnet@hotmail.com>
2020-02-25 15:48:38 -0600

    Tipo de cambio
    Tipo de cambio(queryexecuterSQL,TipocambioAccess...,TipoCambioGiaData...,TipoCambio

---
+++ AppGia/Models/TipoCambioGia.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace AppGia.Models
+{
+    public class TipoCambioGia
+    {
+        public TipoCambioGia()
+        {
+            //Constructor
+        }
+
+        public int id { get; set; }
+        public int anio { get; set; }
+        public int mes { get; set; }
+        public string monedaid { get; set; }
+        public string tipo { get; set; }
+        public string monedareporte { get; set; }
+        public string monedainforme { get; set; }
+        public string usuario { get; set; }
+        public string fecharegistro { get; set; }
+    }
+}
