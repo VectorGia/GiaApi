16167ad9861528105c0db2f9fdb3b526948e5387
hnolasco <hnolasco@getechnologiesmx.com>
2020-02-25 18:49:30 -0600

    ---se agregan hijos a totales y se termina el metodo que regresara los ajustes

--- AppGia/Controllers/ProformaHelper.cs
+++ AppGia/Controllers/ProformaHelper.cs
@@ -99,7 +99,7 @@ namespace AppGia.Controllers
             return detCtas;
         }
 
-        public ProformaDetalle ConstruyeDetalleTotal(List<ProformaDetalle> detalles, Rubros rubroTotal,
+        private ProformaDetalle ConstruyeDetalleTotal(List<ProformaDetalle> detalles, Rubros rubroTotal,
             double porcentaje)
         {
             string aritmetica = rubroTotal.aritmetica;
@@ -148,6 +148,7 @@ namespace AppGia.Controllers
             detalleTotal.nombre_rubro = rubroTotal.nombre;
             detalleTotal.aritmetica = aritmetica;
             detalleTotal.clave_rubro = rubroTotal.clave;
+            detalleTotal.hijos = rubroTotal.hijos;
 
             DataTable dt = new DataTable();
             foreach(var key in keys)
@@ -209,6 +210,19 @@ namespace AppGia.Controllers
         public List<ProformaDetalle> getAjustes(Int64 idCC,Int64 idEmpresa,int anio)
         {
             List<ProformaDetalle> proformaDetalles=new List<ProformaDetalle>();
+            Dictionary<string,string> mesValor=new Dictionary<string, string>();
+            mesValor.Add("1","enero_monto_resultado");
+            mesValor.Add("2","febrero_monto_resultado");
+            mesValor.Add("3","marzo_monto_resultado");
+            mesValor.Add("4","abril_monto_resultado");
+            mesValor.Add("5","mayo_monto_resultado");
+            mesValor.Add("6","junio_monto_resultado");
+            mesValor.Add("7","julio_monto_resultado");
+            mesValor.Add("8","agosto_monto_resultado");
+            mesValor.Add("9","septiembre_monto_resultado");
+            mesValor.Add("10","octubre_monto_resultado");
+            mesValor.Add("11","noviembre_monto_resultado");
+            mesValor.Add("12","diciembre_monto_resultado");
             DataTable ajustesDt = _queryExecuterSql.ExecuteQuerySQL("select ingreso, directo, indirecto, mes " +
                                                                     " from ajuste" +
                                                                     " where empresa = "+idEmpresa +
@@ -230,12 +244,7 @@ namespace AppGia.Controllers
                     Object mesData = ajusteRow["mes"];
                     if (mesData != null)
                     {
-                        Int32 mesAjuste = Convert.ToInt32(mesData);
-                        if (mesAjuste == 1)
-                        {
-                            detalle.enero_monto_resultado = ToDouble(detalle.campoEnAjustes);
-                        }
-                        //aun faltan los demas meses
+                        detalle[mesData.ToString()] = ToDouble(ajusteRow[detalle.campoEnAjustes]);
                     }
                 }
             });
