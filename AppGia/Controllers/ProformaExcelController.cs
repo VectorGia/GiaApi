76aae241b0605df710edf58bbb4bae88c381e0e6
hnolasco <hnolasco@getechnologiesmx.com>
2020-03-09 16:15:24 -0600

    ajustes a nombres

--- AppGia/Controllers/ProformaExcelController.cs
+++ AppGia/Controllers/ProformaExcelController.cs
@@ -13,16 +13,16 @@ namespace AppGia.Controllers
         private ProformaExcelHelper _proformaExcelHelper=new ProformaExcelHelper();
         
         [HttpPost("import")]
-        public List<ProformaDetalle> import(String excelFileB64)
+        public List<ProformaDetalle> import([FromBody]String excelFileB64)
         {
             return _proformaExcelHelper.import(FromBase64String(excelFileB64));
         }
         
         // POST: api/ProformaExcel
         [HttpPost("export")]
-        public string export([FromBody]List<ProformaDetalle> lstGuardaProforma)
+        public string export([FromBody]List<ProformaDetalle> detallesProfToRender)
         {
-            return  ToBase64String(_proformaExcelHelper.export(lstGuardaProforma));
+            return  ToBase64String(_proformaExcelHelper.export(detallesProfToRender));
         }
         
 
