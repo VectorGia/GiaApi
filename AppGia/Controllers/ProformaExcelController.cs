737f35c5b2ef7a48d08f437d1eb3a4b4ffbdc951
hnolasco <hnolasco@getechnologiesmx.com>
2020-03-09 17:32:22 -0600

    ---fix export excel

--- AppGia/Controllers/ProformaExcelController.cs
+++ AppGia/Controllers/ProformaExcelController.cs
@@ -22,7 +22,8 @@ namespace AppGia.Controllers
         [HttpPost("export")]
         public string export([FromBody]List<ProformaDetalle> detallesProfToRender)
         {
-            return  ToBase64String(_proformaExcelHelper.export(detallesProfToRender));
+            string resB64=  ToBase64String(_proformaExcelHelper.export(detallesProfToRender));
+            return resB64;
         }
         
 
