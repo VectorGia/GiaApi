a986211becd92ab19665c6aea914c1d76a48fefd
hnolasco <hnolasco@getechnologiesmx.com>
2020-03-20 10:15:33 -0600

    ----se elimina lo que actualmente estaba sobre la extraccion

--- AppGia/Controllers/ETLController.cs
+++ AppGia/Controllers/ETLController.cs
@@ -1,10 +1,4 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Threading.Tasks;
-using AppGia.Dao;
-using Microsoft.AspNetCore.Http;
-using Microsoft.AspNetCore.Mvc;
+﻿using Microsoft.AspNetCore.Mvc;
 
 namespace AppGia.Controllers
 {
@@ -12,43 +6,10 @@ namespace AppGia.Controllers
     [ApiController]
     public class ETLController : ControllerBase
     {
-        ETLDataAccesLayer objETL = new ETLDataAccesLayer();
-        // GET: api/ETL
-        [HttpGet]
-        public IEnumerable<string> Get()
-        {
-            return new string[] { "value1", "value2" };
-        }
-
-        // GET: api/ETL/5
-        //[HttpGet("{id}", Name = "Get")]
-        public string Get(int id)
-        {
-            return "value";
-        }
-
         // POST: api/ETL
         [HttpPost]
         public void Post([FromBody] string value)
         {
-        }
-
-        // PUT: api/ETL/5
-        [HttpPut("{id}")]
-        public void Put(int id, [FromBody] string value)
-        {
-        }
-
-        // DELETE: api/ApiWithActions/5
-        [HttpDelete("{id}")]
-        public void Delete(int id)
-        {
-        }
-
-        public void GenerarBalanza(int id) {
-
-            objETL.insertarTabBalanza(id);
-
         }
     }
 }
