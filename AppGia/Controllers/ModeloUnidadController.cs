323758ca17727ee995de8f53f2a0625502cc63f6
hnolasco <hnolasco>
2020-04-27 17:35:00 -0500

    ----se agregan servicios para asociacion de modelos y unidades de negocio

---
+++ AppGia/Controllers/ModeloUnidadController.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections.Generic;
+using AppGia.Dao;
+using AppGia.Models;
+using Microsoft.AspNetCore.Mvc;
+
+namespace AppGia.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class ModelounidadController : ControllerBase
+    {
+        private ModeloUnidadNegocioDataAccessLayer objModelo = new ModeloUnidadNegocioDataAccessLayer();
+
+        [HttpGet]
+        public IEnumerable<ModeloUnidadNegocio> Get()
+        {
+            return objModelo.findAll();
+        }
+
+        [HttpGet("{idModelo}/{idUnidad}")]
+        public ModeloUnidadNegocio GetModelo(Int64 idModelo, Int64 idUnidad)
+        {
+            return objModelo.findByIdModeloAndIdUnidad(idModelo, idUnidad);
+        }
+
+        [HttpGet("{idModelo}")]
+        public List<ModeloUnidadNegocio> GetModelo(Int64 idModelo)
+        {
+            return objModelo.findByIdModelo(idModelo);
+        }
+
+        [HttpPost]
+        public int Create([FromBody] ModeloUnidadNegocio modeloUnidadNegocio)
+        {
+            return objModelo.Add(modeloUnidadNegocio);
+        }
+
+
+        [HttpDelete("{idModelo}/{idunidad}")]
+        public int Delete(string id)
+        {
+            ModeloUnidadNegocio modeloUnidadNegocio = new ModeloUnidadNegocio();
+
+            return objModelo.delete(modeloUnidadNegocio);
+        }
+    }
+}
---
+++ AppGia/Models/ModeloUnidadNegocio.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace AppGia.Models
+{
+    public class ModeloUnidadNegocio
+    {
+        public Int64 idModelo { get; set; }
+        public Int64 idUnidad { get; set; }
+        public Boolean activo { get; set; }
+        
+    }
+}
