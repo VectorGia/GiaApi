8edd098307d467a03509b81e7b59952dff0fc0d1
hnolasco <hnolasco>
2020-09-26 02:40:43 -0500

    ----integracion con Acive directory para autenticacion

--- AppGia/Controllers/seguridad/AuthController.cs
+++ AppGia/Controllers/seguridad/AuthController.cs
@@ -34,8 +34,8 @@ namespace AppGia.Controllers
         public IActionResult Login([FromBody] UserModel login)
         {
             IActionResult response = Unauthorized();
-            //var user = AuthenticateUserAD(login);
-            var user = AuthenticateUserDummy(login);
+            var user = AuthenticateUserAD(login);
+            //var user = AuthenticateUserDummy(login);
 
             if (user != null)
             {
@@ -79,118 +79,27 @@ namespace AppGia.Controllers
             return user;
         }
 
-        private UserModel AuthenticateUserNew(UserModel userModel)
-        {
-            try
-            {
-                var cn = new LdapConnection();
-                cn.Connect("10.10.0.102", 389);
-                logger.Info("Active directory connected='{0}'", cn.Connected);
-                String loginDN = "Svc.infogia@gia.mx";
-                String password1 = "1nf0g14202009*";
-
-                cn.Bind(loginDN, password1);
-                logger.Info("Active directory autenticado='{0}'", cn.Bound);
-                cn.Disconnect();
-                return userModel;
-            }
-            catch (Exception e)
-            {
-                logger.Error(e, "Error en autenticacion de active directory");
-                throw;
-            }
-        }
 
         private UserModel AuthenticateUserAD(UserModel userModel)
         {
-            // DirectoryEntry entry = new DirectoryEntry("LDAP://" + domain, userModel.Username, userModel.Password);
-
             string dominio = _config["AD:Dominio"];
-            string path =
-                "LDAP://10.10.0.102/CN=Servicio infogia,OU=Service Accounts,DC=gia,DC=mx"; //_config["AD:Path"];
-            string domainAndUsername = "GIA" + @"\" +"huilver.nolasco"; //dominio + @"\" + userModel.Username;
-            logger.Info("Conectando con AD.....");
-            DirectoryEntry entry = new DirectoryEntry(path, domainAndUsername, "2020*HuNo-Ag___");
-            logger.Info("Conectado con AD --> ok");
+            string path =_config["AD:Path"];
+            string domainAndUsername = dominio + @"\" + userModel.Username;
+            logger.Info("Conectando con ldap. Datos conexion path='{0}',domainAndUsername='{1}' ",path,domainAndUsername);
+            DirectoryEntry entry = new DirectoryEntry(path, domainAndUsername, userModel.Password);
+            logger.Info("Conexion ldap OK");
             try
             {
-                logger.Info("Forzando autenticacion.....>>>> debe fallar");
+                logger.Info("Autenticando domainAndUsername='{0}'",domainAndUsername);
                 object obj = entry.NativeObject;
-                DirectorySearcher search = new DirectorySearcher(entry);
-                logger.Info("Buscando usuario en modelo.....");
-                /*search.Filter = "(SAMAccountName=" + userModel.Username + ")";
-                search.PropertiesToLoad.Add("cn");*/
-                /*SearchResult result = search.FindOne();*/
-                /*logger.Info("Buscando resultado...." + result);*/
-
-
-                //Relacion relacion = new Relacion();
-                //bool existe = new LoginDataAccessLayer().validacionLoginUsuario(relacion, lg);
-
-                /*if (null == result)
-                {
-                    return null;
-                }*/
-                search.Filter = "(&(objectClass=user))";
-                /*search.Filter = "(&(objectClass=user)(objectCategory=person))";*/
-                //search.PropertiesToLoad.Add("samaccountname");
-                SearchResult result;
-                SearchResultCollection resultCol = search.FindAll();
-                if (resultCol != null)
-                {
-                    for (int counter = 0; counter < resultCol.Count; counter++)
-                    {
-                        result = resultCol[counter];
-                        logger.Info("   =====================res '{0}'======================== ",counter);
-                        ResultPropertyCollection myResultPropColl = result.Properties;
-                        foreach( string myKey in myResultPropColl.PropertyNames)  
-                        {  
-                            string tab = "          ";  
-                            logger.Info("     key: "+ myKey + " = ");  
-                            foreach( Object myCollection in myResultPropColl[myKey])  
-                            {  
-                                logger.Info(tab + myCollection);  
-                            }  
-                        }  
-                        
-                        /*if (result.Properties.Contains("samaccountname"))
-                        {
-                            logger.Info("usuario:'{0}'", result.Properties["samaccountname"][0]);
-                        }*/
-                    }
-                }
-
-                return null;
+                logger.Info("Autenticacion ldap OK");
+                return userModel;
             }
             catch (Exception ex)
             {
                 logger.Error(ex, "Error en autenticacion");
                 return null;
             }
-
-            return userModel;
         }
-
-        /*public UserModel AuthenticateUser(UserModel userModel)
-       {
-           string dominio = "infogia";
-           string path = "LDAP://ServerOmnisys/CN=users, DC=Infogia, DC=local";
-           string domainAndUsername = dominio + @"\" + userModel.Username;
-           DirectoryEntry entry = new DirectoryEntry(path, domainAndUsername, userModel.Password);
-           try
-           {
-               DirectorySearcher dirSearcher = new DirectorySearcher(entry);
-               //dirSearcher.Filter = "(&(objectClass=user)(objectCategory=person))";
-               dirSearcher.FindOne();
-               //Relacion relacion = new Relacion();
-               //bool existe = new LoginDataAccessLayer().validacionLoginUsuario(relacion, lg);
-               return userModel;
-           }
-           catch (Exception ex)
-           {
-               logger.Error(ex, "Error de autenticacion");
-               return null;
-           }
-       }*/
     }
 }
